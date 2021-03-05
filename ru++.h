c52511b17d637e8821346156ca0adc5abe89437c
CatYoutuber <66025258+CatYoutuber@users.noreply.github.com>
2021-03-05 18:06:03 +0200

    Add files via upload

---
+++ ru++.h
@@ -0,0 +1,26 @@
+//Translated keywords by CatYoutuber
+
+#define воид void
+#define клас class
+#define енум enum
+#define структ struct
+#define инт int
+#define чар char
+#define ретурн return
+#define нулл nullptr
+#define нью new
+#define делит delete
+#define публик public
+#define приват private
+#define статик static
+#define волатайл volatile
+#define фор for
+#define вайл while
+#define брейк break
+#define бул bool
+#define трай try
+#define кетч catch
+#define иф if
+#define елсе else
+#define неймспейс namespace
+#define абстракт abstract
